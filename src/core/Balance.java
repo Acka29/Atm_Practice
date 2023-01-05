ea8decc036450894588ad7d7c1d66a4bf7261e46
aleksandrajonoska <jonoska.aleksandra@yahoo.com>
2023-01-05 09:55:08 -0700

    Balance page added

---
+++ src/core/Balance.java
@@ -0,0 +1,13 @@
+package core;
+
+public class Balance {
+
+    public static void main(String[] args) {
+
+        System.out.println("Balance");
+        System.out.println("Withdraw");
+        System.out.println("Deposit");
+
+
+    }
+}
