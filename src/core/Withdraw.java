e4257987cc532107fd5fccb0d37267abc1a7a5d9
aleksandrajonoska <jonoska.aleksandra@yahoo.com>
2023-01-05 10:12:49 -0700

    Withdraw page added

---
+++ src/core/Withdraw.java
@@ -0,0 +1,11 @@
+package core;
+
+public class Withdraw {
+
+    public static void main(String[] args) {
+
+        System.out.println("How much would you like to withdraw");
+        System.out.println("$500");
+
+    }
+}
